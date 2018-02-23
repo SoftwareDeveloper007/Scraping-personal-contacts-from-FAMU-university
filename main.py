33268b2aa51e3244c73c8e5ca828b475caae338f
Wesche D <28940651+ScrapingExpert007@users.noreply.github.com>
2018-02-23 06:07:49 -0800

    2nd Commit

--- main.py
+++ main.py
@@ -12,7 +12,16 @@ from datetime import date, datetime, timedelta
 from dateutil.relativedelta import *
 
 
-def scraping(last_name="", first_name=""):
+def scraping(first_name="", last_name=""):
 
     url = "http://www.famu.edu/directory/employeesearch.cfm"
+    url = url + "?srch={}+{}&submit=Search".format(first_name, last_name)
+
+    r = requests.post(url)
     
+    html = r.content.decode()
+
+    print(html)
+
+
+scraping(first_name="RANDALL", last_name="ABATE")
